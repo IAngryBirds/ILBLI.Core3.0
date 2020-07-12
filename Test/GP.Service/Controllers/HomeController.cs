88048cf1a28effb847586b130adfd1200a8f6386
IAngryBirds <zhangjie839x@163.com>
2020-07-12 18:23:57 +0800

    1

---
+++ Test/GP.Service/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GP.Service.Models;
+using System.Net.Http;
+using System.IO;
+using System.Text;
+
+namespace GP.Service.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _HttpClientFactory;
+        public HomeController(IHttpClientFactory clientFactory)
+        {
+            this._HttpClientFactory = clientFactory;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
